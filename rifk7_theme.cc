825ad5d4c66f27089bbd14fc4e8da9a0deaaf635
Spoof <76165775+WhyWKey@users.noreply.github.com>
2023-05-25 20:01:32 +0330

    Create rifk7.theme.cc

---
+++ rifk7_theme.cc
@@ -0,0 +1,62 @@
+/**
+ * @name rifk7theme
+ * @author Spoof#8772
+ * @version 1
+ * @website https://terrorist.bio/spoof
+ * @authorId 700974854605766737
+*/
+
+@import url('https://discordstyles.github.io/FrostedGlass/dist/FrostedGlass.css');
+@import url('https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css');
+@import url('https://nyri4.github.io/Discolored/main.css');
+@import url('https://discordstyles.github.io/Addons/windows-titlebar.css');
+
+:root {
+  --background-image: url('https://cdn.discordapp.com/attachments/1011493326094086204/1111327563273474139/rifk.png');
+  --background-image-blur: 30px;
+  --background-image-size: cover;
+  --background-image-position: center;
+  --popout-image: url('https://cdn.discordapp.com/attachments/1011493326094086204/1111327563273474139/rifk.png');
+  --popout-image-blur: 0px;
+  --popout-image-size: auto;
+  --popout-image-position: center;
+  --overlay-image: url('https://cdn.discordapp.com/attachments/1011493326094086204/1111327563273474139/rifk.png');
+  --overlay-image-blur: 5px;
+  --overlay-image-size: cover;
+  --overlay-image-position: center;
+  --home-button-image: url('https://cdn.discordapp.com/attachments/1011493326094086204/1111327563273474139/rifk.png');
+  --home-button-size: 100%;
+  --home-button-position: center;
+  --serverlist-brightness: 0;
+  --left-brightness: 0;
+  --middle-brightness: 0;
+  --right-brightness: 0;
+  --overlay-brightness: 0;
+  --gradient-primary: 242,7,248;
+  --gradient-secondary: 95,241,43;
+  --link-colour: #CE7AA4;
+  --scrollbar-colour: rgb(46,4,92,1);
+  --gradient-direction: 300deg;
+  --tint-colour: 19,6,33;
+  --tint-brightness: 0.75;
+  --font: gg sans;
+  --window-padding: 0px;
+  --window-roundness: 0px;
+  --update-notice-1: none;
+  --rs-small-spacing: 2px;
+  --rs-medium-spacing: 2px;
+  --rs-large-spacing: 2px;
+  --rs-small-width: 2px;
+  --rs-medium-width: 2px;
+  --rs-large-width: 2px;
+  --rs-avatar-shape: 50%;
+  --rs-online-color: #43b581;
+  --rs-idle-color: #faa61a;
+  --rs-dnd-color: #f04747;
+  --rs-offline-color: #636b75;
+  --rs-streaming-color: #643da7;
+  --rs-phone-visible: block;
+}
+
+/* Any custom CSS below here */
+
